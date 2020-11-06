a84d2eab3424d3f208c0f34d9fcafe3683cad76e
Armando Chindoy <armandojchinody@gmail.com>
2020-11-06 11:03:32 -0500

    Updates

---
+++ precache-manifest.c596d559129c21467e154b9191b34491.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c54174a10cbdfbfebeb5",
+    "url": "/static/css/main.73bfe5e2.chunk.css"
+  },
+  {
+    "revision": "c54174a10cbdfbfebeb5",
+    "url": "/static/js/main.c54174a1.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0a30f79a6c252437c567",
+    "url": "/static/css/2.7ba8a2cf.chunk.css"
+  },
+  {
+    "revision": "0a30f79a6c252437c567",
+    "url": "/static/js/2.0a30f79a.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "5861c44db520a839676b8df01f7c2213",
+    "url": "/static/media/loader.5861c44d.gif"
+  },
+  {
+    "revision": "d85a0be92bbce44a74965b2f6684565c",
+    "url": "/static/media/404Error.d85a0be9.svg"
+  },
+  {
+    "revision": "044973fd1d313ace4c7cedf78676d24f",
+    "url": "/static/media/logo.044973fd.svg"
+  },
+  {
+    "revision": "c24e6dd10b9960af5f45bc6156d97448",
+    "url": "/static/media/stars.c24e6dd1.svg"
+  },
+  {
+    "revision": "8f97413ce8861052b5ce229f516aada6",
+    "url": "/index.html"
+  }
+];
